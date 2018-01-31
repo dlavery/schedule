91fe87d2b8acf03e52e97a834376dcd9ac5876ea
David Lavery <dlavery62@gmail.com>
2018-01-31 08:02:18 +0000

    unit tests

--- schedule.py
+++ schedule.py
@@ -1,117 +1,146 @@
 import sys
+import os
 import json
 from datetime import date
 from datetime import datetime
 from datetime import timedelta
 from monthdelta import monthdelta
 
-##########################################
-# FUNCTIONS
-##########################################
-# Create a CPS format string from a date
-def formet_as_cps_date(d):
-    if d:
-        return d.strftime('%d-%b-%Y') # e.g. 24-Jan-2018
-    else:
-        return ''
+class Schedule:
 
-# Create a date from a CPS format string
-def date_from_cps_format(cps):
-    if cps:
-        return datetime.strptime(cps, '%d-%b-%Y')
-    else:
-        return ''
+    def __init__(self):
+        self.processing_days = {}
+        try:
+            os.remove('payments.csv')
+        except FileNotFoundError:
+            pass
 
-# Create an ISO date format string from a date
-def format_as_iso_date(d):
-    if d:
-        return d.strftime('%Y-%m-%d')
-    else:
-        return ''
-
-# Create a date from an ISO format string
-def date_from_iso_format(iso):
-    if iso:
-        return datetime.strptime(iso,'%Y-%m-%d')
-    else:
-        return ''
-
-# Make a payment
-def make_payment(schedule, reference):
-    print('PAYMENT: '
-    + schedule['payee']['name'] + '|'
-    + schedule['payee']['sortCode'] + '|'
-    + schedule['payee']['account'] + '|'
-    + reference + '|'
-    + str('%.2f' % schedule['amount']))
-
-
-##########################################
-# MAIN SCRIPT
-##########################################
-# Get CPS payment schedule into a dictionary (map).
-# Schedule format is: 'due from date', 'due to date', 'process date'
-processing_days = {}
-f1 = open('processing_days.csv', 'r')
-for line in f1:
-    l = line[:-1].split(',')
-    processing_days[l[0]] = (l[1], l[2])
-
-# Assuming last processing was yesterday, work out whether
-# any payments should be processed today
-now = datetime.now()
-itr_date = now
-todays_date = formet_as_cps_date(now)
-process_to_due_date = None
-while True:
-    k = formet_as_cps_date(itr_date)    # create the key to lookup day
-    if k in processing_days:            # lookup day in processing days
-        # check if payments due on day k should be processed today
-        if processing_days[k][1] == todays_date:
-            tmp = date_from_cps_format(processing_days[k][0])
-            if (not process_to_due_date or process_to_due_date < tmp):
-                process_to_due_date = tmp
+    # Create a CPS format string from a date
+    def formet_as_cps_date(self, d):
+        if d:
+            return d.strftime('%d-%b-%Y') # e.g. 24-Jan-2018
         else:
-            tmp = date_from_cps_format(processing_days[k][1])
-            # if the new process date is in the future, then we are done today
-            if tmp > now:
+            return ''
+
+    # Create a date from a CPS format string
+    def date_from_cps_format(self, cps):
+        if cps:
+            return datetime.strptime(cps, '%d-%b-%Y')
+        else:
+            return ''
+
+    # Create an ISO date format string from a date
+    def format_as_iso_date(self, d):
+        if d:
+            return d.strftime('%Y-%m-%d')
+        else:
+            return ''
+
+    # Create a date from an ISO format string
+    def date_from_iso_format(self, iso):
+        if iso:
+            return datetime.strptime(iso,'%Y-%m-%d')
+        else:
+            return ''
+
+    # Make a payment
+    def make_payment(self, schedule, reference, fh):
+        s = schedule['payee']['name'] + '|' \
+        + schedule['payee']['sortCode'] + '|' \
+        + schedule['payee']['account'] + '|' \
+        + reference + '|' + \
+        str('%.2f' % schedule['amount'])
+        fh.write(s + '\n')
+        print('PAYMENT: ' + s)
+
+    # Get CPS payment schedule into a dictionary (map).
+    # Schedule format is: 'due from date', 'due to date', 'process date',
+    # index is 'due from date'
+    def get_schedule(self):
+        f1 = open('processing_days.csv', 'r')
+        for line in f1:
+            l = line[:-1].split(',')
+            self.processing_days[l[0]] = (l[1], l[2])
+        f1.close()
+
+    # Find the future date where payments due up to and including
+    # this date should be processed today
+    def find_forward_date(self):
+        now = datetime.now()
+        itr_date = now
+        todays_date = self.formet_as_cps_date(now)
+        process_to_due_date = None
+        while True:
+            k = self.formet_as_cps_date(itr_date)    # create the key to lookup day
+            if k in self.processing_days:            # lookup day in processing days
+                # check if payments due on day k should be processed today
+                if self.processing_days[k][1] == todays_date:
+                    tmp = self.date_from_cps_format(self.processing_days[k][0])
+                    if (not process_to_due_date or process_to_due_date < tmp):
+                        process_to_due_date = tmp
+                else:
+                    tmp = self.date_from_cps_format(self.processing_days[k][1])
+                    # if the new process date is in the future, then we are done today
+                    if tmp > now:
+                        break
+                itr_date = itr_date + timedelta(days=1)
+            else:   # something wrong if we can't find the date in our processing days
                 break
-        itr_date = itr_date + timedelta(days=1)
-    else:   # something wrong if we can't find the date in our processing days
-        break
+        return process_to_due_date
 
-# If payments should be processed today then process_to_due_date will be set
-if not process_to_due_date:
-    print('Not a processing day, can not continue')
-    sys.exit()
+    # Assuming last processing was yesterday, work out whether
+    # any payments should be processed today and process them
+    def process(self):
+        payment_file = open('payments.csv','w')
+        self.get_schedule()
+        now = datetime.now()
+        itr_date = now
+        todays_date = self.formet_as_cps_date(now)
+        process_to_due_date = self.find_forward_date()
 
-# We now process all payments due up to and including process_to_due_date
-print('Processing up to: ' + formet_as_cps_date(process_to_due_date))
-# Load the schedules from file
-data = json.load(open('schedules.json'))
-for schedule in data['schedules']:
-    freq_period = schedule['frequency'][1:].upper()
-    freq_number = int(schedule['frequency'][0:1])
-    if freq_period == 'W':
-        delta = timedelta(days=(7 * freq_number))
-    elif freq_period == 'M':
-        delta = monthdelta(freq_number)
-    else:
-        continue    # not a valid schedule
-    processed_up_to = date_from_iso_format(schedule['processedUpTo'])
-    if processed_up_to == '':
-        tmp = date_from_iso_format(schedule['paymentStartDate'])
-    else:
-        tmp = processed_up_to + delta
-    while True:
-        if (tmp <= process_to_due_date and tmp <= date_from_iso_format(schedule['paymentEndDate'])):
-            make_payment(schedule, tmp.strftime('%d-%b') + ' PAYMENT' )
-            processed_up_to = tmp
-            tmp = processed_up_to + delta
-        else:
-            # update the schedule
-            schedule['processedUpTo'] = format_as_iso_date(processed_up_to)
-            break
+        # If payments should be processed today then process_to_due_date will be set
+        if not process_to_due_date:
+            print('Not a processing day, can not continue')
+            return
 
-# Lastly write the schedules back to file
-json.dump(data, open('schedules_update.json', 'w'), indent=2)
+        # We now process all payments due up to and including process_to_due_date
+        print('Processing up to: ' + self.formet_as_cps_date(process_to_due_date))
+        # Load the schedules from file
+        f1 = open('schedules.json')
+        data = json.load(f1)
+        f1.close()
+        for schedule in data['schedules']:
+            freq_period = schedule['frequency'][1:].upper()
+            freq_number = int(schedule['frequency'][0:1])
+            if freq_period == 'W':
+                delta = timedelta(days=(7 * freq_number))
+            elif freq_period == 'M':
+                delta = monthdelta(freq_number)
+            else:
+                continue    # not a valid schedule
+            processed_up_to = self.date_from_iso_format(schedule['processedUpTo'])
+            if processed_up_to == '':
+                tmp = self.date_from_iso_format(schedule['paymentStartDate'])
+            else:
+                tmp = processed_up_to + delta
+            while True:
+                if (tmp <= process_to_due_date and tmp <= self.date_from_iso_format(schedule['paymentEndDate'])):
+                    self.make_payment(schedule, tmp.strftime('%d-%b') + ' PAYMENT', payment_file)
+                    processed_up_to = tmp
+                    tmp = processed_up_to + delta
+                else:
+                    # update the schedule
+                    schedule['processedUpTo'] = self.format_as_iso_date(processed_up_to)
+                    break
+
+        # Close payments file
+        payment_file.close()
+
+        # Lastly write the schedules back to file
+        f2 = open('schedules_update.json', 'w')
+        json.dump(data, f2, indent=2)
+        f2.close()
+
+if __name__ == '__main__':
+    sched = Schedule()
+    sched.process()
---
+++ testschedule.py
@@ -0,0 +1,155 @@
+import unittest
+import json
+from datetime import date
+from datetime import timedelta
+from monthdelta import monthdelta
+from schedule import Schedule
+
+class TestSchedule(unittest.TestCase):
+
+    def setUp(self):
+        data = {}
+        data['schedules'] = []
+        todays_date = date.today()
+
+        # Schedule 1 - multiple weekly payments due
+        base_date = todays_date + timedelta(days=2)
+        data['schedules'].append({ \
+            "payee": {"name": "Jane Doe", "sortCode": "01-01-01", "account": "12345678"}, \
+            "amount": 123.4, \
+            "frequency": "1W", \
+            "paymentStartDate": (base_date - timedelta(days=28)).isoformat(), \
+            "paymentEndDate": (base_date + timedelta(days=364)).isoformat(), \
+            "processedUpTo": (base_date - timedelta(days=21)).isoformat() \
+        })
+
+        # Schedule 2 - no payments due
+        base_date = todays_date - timedelta(days=7)
+        data['schedules'].append({ \
+            "payee": {"name": "John Doe", "sortCode": "02-02-02", "account": "23456789"}, \
+            "amount": 155.34, \
+            "frequency": "1M", \
+            "paymentStartDate": base_date.isoformat(), \
+            "paymentEndDate": (base_date + timedelta(days=365)).isoformat(), \
+            "processedUpTo": base_date.isoformat() \
+        })
+
+        # Schedule 3 - 1 monthly payment due
+        base_date = todays_date + timedelta(days=1)
+        data['schedules'].append({ \
+            "payee": {"name": "Bob Smith", "sortCode": "03-03-03", "account": "34567890"}, \
+            "amount": 134, \
+            "frequency": "1M", \
+            "paymentStartDate": (base_date - monthdelta(3)).isoformat(), \
+            "paymentEndDate": (base_date + monthdelta(9)).isoformat(), \
+            "processedUpTo": (base_date - monthdelta(1)).isoformat() \
+        })
+
+        # Schedule 4 - Brand new weekly schedule, 1 payment due
+        base_date = todays_date
+        data['schedules'].append({ \
+            "payee": {"name": "Alice Smith", "sortCode": "04-04-04", "account": "45678901"}, \
+            "amount": 151.01, \
+            "frequency": "1W", \
+            "paymentStartDate": base_date.isoformat(), \
+            "paymentEndDate": (base_date + timedelta(days=365)).isoformat(), \
+            "processedUpTo": "" \
+        })
+
+        # Schedule 5 - Brand new weekly schedule, no payment due
+        base_date = todays_date + timedelta(days=6)
+        data['schedules'].append({ \
+            "payee": {"name": "Joe Jones", "sortCode": "05-05-05", "account": "56789012"}, \
+            "amount": 78.3, \
+            "frequency": "1W", \
+            "paymentStartDate": base_date.isoformat(), \
+            "paymentEndDate": (base_date + timedelta(days=365)).isoformat(), \
+            "processedUpTo": "" \
+        })
+
+        # Schedule 6 - 1 four-weekly payment due
+        base_date = todays_date + timedelta(days=2)
+        data['schedules'].append({ \
+            "payee": {"name": "Jo Jones", "sortCode": "06-06-06", "account": "67890123"}, \
+            "amount": 176.21, \
+            "frequency": "4W", \
+            "paymentStartDate": (base_date - timedelta(weeks=8)).isoformat(), \
+            "paymentEndDate": (base_date + timedelta(weeks=44)).isoformat(), \
+            "processedUpTo": (base_date - timedelta(weeks=4)).isoformat() \
+        })
+
+        # Schedule 7 - no payments due (sometimes)
+        #base_date = todays_date + timedelta(days=3)
+        #data['schedules'].append({ \
+        #    "payee": {"name": "Harry X", "sortCode": "07-07-07", "account": "78901234"}, \
+        #    "amount": 119.35, \
+        #    "frequency": "1W", \
+        #    "paymentStartDate": (base_date - timedelta(weeks=1)).isoformat(), \
+        #    "paymentEndDate": (base_date + timedelta(weeks=51)).isoformat(), \
+        #    "processedUpTo": (base_date - timedelta(weeks=1)).isoformat() \
+        #})
+
+        fh = open('schedules.json', 'w')
+        json.dump(data, fh, indent=2)
+        fh.close()
+
+    def test_schedule(self):
+        sched = Schedule()
+        sched.process()
+        # check payments
+        f1 = open('payments.csv', 'r')
+        todays_date = date.today()
+        # expect 3 equal payments to Jane Doe
+        i = 0
+        while i < 3:
+            l = f1.readline()[:-1].split('|')
+            self.assertEqual(l[0], 'Jane Doe')
+            self.assertEqual(l[4], '123.40')
+            i = i + 1
+        # expect 1 payment to Bob Smith
+        l = f1.readline()[:-1].split('|')
+        self.assertEqual(l[0], 'Bob Smith')
+        self.assertEqual(l[4], '134.00')
+        # expect 1 payment to Alice Smith
+        l = f1.readline()[:-1].split('|')
+        self.assertEqual(l[0], 'Alice Smith')
+        self.assertEqual(l[4], '151.01')
+        # expect 1 payment to Jo Jones
+        l = f1.readline()[:-1].split('|')
+        self.assertEqual(l[0], 'Jo Jones')
+        self.assertEqual(l[4], '176.21')
+        # no more payments
+        l = f1.readline()[:-1].split('|')
+        self.assertEqual(l, [''])
+
+        f1.close()
+
+        # check new schedules
+        f2 = open('schedules_update.json', 'r')
+        data = json.load(f2)
+        # expect processed up to date for Jane Doe is today + 2 days
+        self.assertEqual(data['schedules'][0]['payee']['name'], 'Jane Doe')
+        self.assertEqual(data['schedules'][0]['processedUpTo'], (todays_date + timedelta(days=2)).isoformat())
+        # expect processed up to date for John Doe is unchanged
+        self.assertEqual(data['schedules'][1]['payee']['name'], 'John Doe')
+        self.assertEqual(data['schedules'][1]['processedUpTo'], (todays_date - timedelta(days=7)).isoformat())
+        # expect processed up to date for Bob Smith is today + 1 day
+        self.assertEqual(data['schedules'][2]['payee']['name'], 'Bob Smith')
+        self.assertEqual(data['schedules'][2]['processedUpTo'], (todays_date + timedelta(days=1)).isoformat())
+        # expect processed up to date for Alice Smith is today
+        self.assertEqual(data['schedules'][3]['payee']['name'], 'Alice Smith')
+        self.assertEqual(data['schedules'][3]['processedUpTo'], todays_date.isoformat())
+        # expect processed up to date for Joe Jones is empty
+        self.assertEqual(data['schedules'][4]['payee']['name'], 'Joe Jones')
+        self.assertEqual(data['schedules'][4]['processedUpTo'], '')
+        # expect processed up to date for Jo Jones is today + 2
+        self.assertEqual(data['schedules'][5]['payee']['name'], 'Jo Jones')
+        self.assertEqual(data['schedules'][5]['processedUpTo'], (todays_date + timedelta(days=2)).isoformat())
+        # expect processed up to date for Harry X is unchanged (sometimes)
+        #self.assertEqual(data['schedules'][6]['payee']['name'], 'Harry X')
+        #self.assertEqual(data['schedules'][6]['processedUpTo'], (todays_date - timedelta(days=4)).isoformat())
+
+        f2.close()
+
+if __name__ == '__main__':
+    unittest.main()
